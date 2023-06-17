ad4263db3c77722bff41f81c10bc3fa3190db59e
Waskito Pringgohandoko <waskito.ph@gmail.com>
2023-06-18 01:34:37 +0700

    aaaaak

--- streamlit_app.py
+++ streamlit_app.py
@@ -60,7 +60,7 @@ except URLError as  e:
 streamlit.header("The fruit load list contains")
 
 #add button
-if streamlit.button('Get Fruit  Load List'):
+if streamlit.button('Get Fruit List'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     my_data_rows = get_fruit_load_list()
     my_cnx.close()
