d4758d06222c4de46d4b1897e22bec31290302f7
Waskito Pringgohandoko <waskito.ph@gmail.com>
2023-06-18 01:23:52 +0700

    added button to get fruit list

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,6 +34,10 @@ def get_fruityvice_data(this_fruit_choice):
     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
     return fruityvice_normalized
 
+def get_fruit_load_list():
+    with my_cnx.cursor() as my_cur:
+        my_cur.execute("select * from fruit_load_list")
+        return my_cur.fetchall()
 
 #fruityvice
 streamlit.header('Fruityvice Fruit Advice!')
@@ -48,20 +52,18 @@ try:
 except URLError as  e:
     streamlit.error
 
+streamlit.header("The fruit load list contains")
+
+#add button
+if streamlit.button('Get Fruit  Load List'):
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    #my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+    my_data_rows = get_fruit_load_list()
+    #streamlit.text("Hello from Snowflake:")
+    streamlit.dataframe(my_data_rows)
 
 #don't run anything past here while we troubleshoot
 streamlit.stop()
-
-
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-#my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-my_cur.execute("select * from fruit_load_list")
-#my_data_row = my_cur.fetchone()
-my_data_rows = my_cur.fetchall()
-#streamlit.text("Hello from Snowflake:")
-streamlit.header("The fruit load list contains")
-streamlit.dataframe(my_data_rows)
 
 add_my_fruit = streamlit.text_input('What fruit would you like to add')
 streamlit.write('Thanks for adding ', add_my_fruit)
