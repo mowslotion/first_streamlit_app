66d18276eaaf8b0809ba6fe137eada5b340e3ef1
Waskito Pringgohandoko <waskito.ph@gmail.com>
2023-06-18 01:37:15 +0700

    fix insert query

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,7 +41,7 @@ def get_fruit_load_list():
     
 def insert_row_snowflake(new_fruit):
     with my_cnx.cursor() as my_cur:
-        my_cur.execute("insert into fruit_load_list values '"+ new_fruit + '"')
+        my_cur.execute("insert into fruit_load_list values ('"+ new_fruit + "')")
         streamlit.write('Thanks for adding ', new_fruit)
 
 #fruityvice
